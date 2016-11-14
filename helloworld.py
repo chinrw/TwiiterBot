01552baacf2946806a48e70495b20c7e63b99e47
chin39 <chin39@virtual.chin.com>
2016-11-13 22:06:41 -0500

    test tweepy

--- helloworld.py
+++ helloworld.py
@@ -2,30 +2,24 @@
 # encoding: utf-8
 
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import tweepy
 
-import tweepy, time, sys
-
-argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'uUMUkkpfX5UVdZfWaiJSW5ZTZ'
+CONSUMER_KEY = 'ftpSZnEi0f3JnoaBXtCg2BxH0'
 #keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '38HfvjKXzDaBfPusjjGdefWmXI01ZXwjo2Er3txZtyR1il8i45'
+CONSUMER_SECRET = 'VX2zyXVqyhYGVsj5doIOho8VEjqQKPXrChENchyBKnzU1tWQVJ'
 #keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '3198328536-MfZV85Q3DYokksNEAYrLR4lGnAs2h60MfuD5sit'
+ACCESS_KEY = '796182382286163968-2FwcfnMsnNwvduip8Juyw9QD7N6dCFj'
 #keep the quotes, replace this with your access token
-ACCESS_SECRET = 'jFy2f72bfLNcW8zKmVeuCu5blsu2OoJ7moT3RXHpxUedn'
+ACCESS_SECRET = 'gfB2FXA5eho99pU8ZZ7RkxZI6vr8OXlgzE7LEfzx0d5XX'
 #keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+
 api = tweepy.API(auth)
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
-
-for line in f:
-    api.update_status(line)
-    time.sleep(900)#Tweet every 15 minutes
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
