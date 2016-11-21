79c5590ae8c49ac985b5dacfab90ab9b766a4eef
chin39 <chin39@virtual.chin.com>
2016-11-21 00:43:14 -0500

    Now can put the weather data to twitter

--- GetWeather_Data.py
+++ GetWeather_Data.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import urllib2,urllib
 import json
+import urllib
+import urllib2
 
-def get_weather_Data(cityid, api_key):
+
+def get_weather_Data(api_key):
     # return a dict data that coutains required information
 
-    url = 'http://api.wunderground.com/api/%s/hourly/q/NY/Troy.json'\
-        %(api_key)
+    url = 'http://api.wunderground.com/api/%s/conditions/q/NY/Troy.json'\
+        % (api_key)
     req = urllib2.Request(url)
     content = urllib2.urlopen(req).read()
     data = json.loads(content)
@@ -18,4 +20,4 @@ def get_weather_Data(cityid, api_key):
 if __name__ == "__main__":
     cityid = 5141502
     api_key = '3dfa9f9c19a712ac'
-    get_weather_Data(cityid, api_key)
+    get_weather_Data(api_key)
--- WeatherBot_Auth.py
+++ WeatherBot_Auth.py
@@ -2,25 +2,32 @@
 # encoding: utf-8
 
 
-import tweepy
 import re
 
+import tweepy
 
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'ftpSZnEi0f3JnoaBXtCg2BxH0'
-#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'VX2zyXVqyhYGVsj5doIOho8VEjqQKPXrChENchyBKnzU1tWQVJ'
-#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '796182382286163968-2FwcfnMsnNwvduip8Juyw9QD7N6dCFj'
-#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'gfB2FXA5eho99pU8ZZ7RkxZI6vr8OXlgzE7LEfzx0d5XX'
-#keep the quotes, replace this with your access token secret
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+from GetWeather_Data import *
 
-
+# enter the corresponding information from your twitter application:
+consumer_key = 'ftpSZnEi0f3JnoaBXtCg2BxH0'
+# keep the quotes, replace this with your consumer key
+consumer_secret = 'VX2zyXVqyhYGVsj5doIOho8VEjqQKPXrChENchyBKnzU1tWQVJ'
+# keep the quotes, replace this with your consumer secret key
+access_key = '796182382286163968-2FwcfnMsnNwvduip8Juyw9QD7N6dCFj'
+# keep the quotes, replace this with your access token
+access_secret = 'gfB2FXA5eho99pU8ZZ7RkxZI6vr8OXlgzE7LEfzx0d5XX'
+# keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
-highpoints = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
-public_tweets = api.home_timeline()
-word = "just a test"
-api.update_status(status = word)
+
+if __name__ == "__main__":
+    api_key = '3dfa9f9c19a712ac'
+    data = get_weather_Data(api_key)
+    location = data["current_observation"]["display_location"]["full"]
+    wind = data["current_observation"]['windchill_string']
+    temp = data["current_observation"]['temperature_string']
+    time = data["current_observation"]["observation_time"]
+    tweet_output = location + '\n' + time + '\n' + temp + '\n' + wind + "\n"
+
+    api.update_status(status=tweet_output)
