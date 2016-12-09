2389b3f5457bbd2c39bce2880f13ad40ed5d2161
nsirano <nsirano@gmail.com>
2016-12-09 10:40:06 -0500

    Added usage info to support scheduled tweets.

--- usage.py
+++ usage.py
@@ -6,10 +6,12 @@ def usage():
     '''
     Prints usage for program.
     '''
-    print("Usage: ./%s <keys.txt>"%(sys.argv[0]))
+    print("Usage: ./%s <keys.txt> <minutes>"%(sys.argv[0]))
     print("    keys.txt should have only the following on each respective line:")
     print("    Line 1:    'Twitter consumer key'")
     print("    Line 2:    'Twitter consumer secret'")
     print("    Line 3:    'Twitter access key'")
     print("    Line 4:    'Twitter access secret'")
     print("    Line 5:    'WUnderground API key'")
+    print(" ")
+    print("    minutes  -  Tweet interval in minutes, must be >15")
