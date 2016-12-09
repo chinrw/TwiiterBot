6ce2587e23b32f7d3212148060d0be46cb70ace1
nsirano <nsirano@gmail.com>
2016-12-09 11:17:35 -0500

    Bug fixes and added code for tweet scheduling.

--- WeatherBot.py
+++ WeatherBot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import sys
-import requests
 import os
+import requests
+import sched
+import sys
+import time
 
 from GetWeather_Data import get_weather_data
 from WeatherBot_Auth import authenticate
@@ -17,7 +19,8 @@ def arg_check():
         usage()
         sys.exit(1)
 
-def get_current_weather(request_type, location):
+def get_current_weather(request_type="conditions",\
+                        location="/q/zmw:12180.1.99999"):
     """
     Returns a string with current weather information for tweet.
     """
@@ -38,28 +41,40 @@ def get_current_weather(request_type, location):
     icon_file = 'temp.jpg'
     request = requests.get(icon_url, stream=True)
     if request.status_code == 200:
-        with open(filename, 'wb') as image:
+        with open(icon_file, 'wb') as image:
             for chunk in request:
                 image.write(chunk)
 
     tweet_text = location + '\n' \
                + time     + '\n' \
                + temp     + '\n' \
-               + "Feels like " + windchill + '\n'
+               + "Feels like " + windchill + '\n'\
                + wind + '\n'
 
     return twitter, icon_file, tweet_text
 
-def tweet(twitter_api, text, image_file=""):
+def tweet():
     """
     Tweets from text and image input.
     """
-    if len(image_file) > 0:
-        twitter_api.update_with_media(filename=icon_file, status=text)
+    twitter_api, icon_file, tweet_text = get_current_weather()
+
+    print(tweet_text)
+
+    if len(icon_file) > 0:
+        twitter_api.update_with_media(filename=icon_file, status=tweet_text)
         os.remove(filename)
 
     else:
-        twitter_api.update(status=text)
+        twitter_api.update(status=tweet_text)
+
+def scheduled_tweet(minutes):
+    """
+    Tweets iteratively every given amount of minutes.
+    """
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(60*minutes, 1, tweet, ())
+    s.run()
 
 def run():
     """
@@ -68,14 +83,12 @@ def run():
     """
     arg_check()
 
-    request_type = "conditions"
-    location = '/q/zmw:12180.1.99999'
+    if len(sys.argv) > 2:
+        minutes = sys.argv[2]
+        scheduled_tweet(minutes)
 
-    twitter, icon_file, tweet_text = get_current_weather(request_type, location)
-
-    print(tweet_text)
-
-    tweet(twitter, tweet_text, icon_file)
+    else:
+        tweet()
 
 # ============================================================================ #
 if __name__ == "__main__":
